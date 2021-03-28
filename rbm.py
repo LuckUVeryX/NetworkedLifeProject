ee177f56058d09e72b5c6da799634205a74c68ee
LuckUVeryX <yip.weisheng.ryan.2014@vjc.sg>
2021-03-28 14:35:03 +0800

    [fix] made initialLearningRate to reflect actual learning rate

--- rbm.py
+++ rbm.py
@@ -190,4 +190,4 @@ def getAdaptiveLearningRate(lr0, epoch):
     # https://towardsdatascience.com/learning-rate-schedules-and-adaptive-learning-rate-methods-for-deep-learning-2c8f433990d1
     # TODO tune hyper parameter k
     k = 4  # * k is a hyper parameter
-    return lr0/(1+k*epoch)
+    return lr0/(1+k*(epoch-1))
